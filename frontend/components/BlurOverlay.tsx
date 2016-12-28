295d971f9b683bbbf9bf91434114f2cba254ee83
Sean Kelley <seansfkelley@gmail.com>
2016-12-28 03:39:31 -0500

    Darken the background blur so I can have light elements above it.

--- frontend/components/BlurOverlay.tsx
+++ frontend/components/BlurOverlay.tsx
@@ -4,13 +4,19 @@ import * as classNames from 'classnames';
 interface Props {
   background: React.ReactNode;
   foreground?: React.ReactNode;
+  isDark?: boolean;
 }
 
 export default class BlurOverlay extends React.PureComponent<Props, void> {
+  static defaultProps = {
+    isDark: true
+  };
+
   render() {
     return (
       <div className={classNames('blur-overlay', { 'with-foreground': !!this.props.foreground })}>
         <div className='child-container background'>{this.props.background}</div>
+        {this.props.isDark ? <div className='dark-overlay'/> : null}
         <div className='child-container foreground'>{this.props.foreground}</div>
       </div>
     );
