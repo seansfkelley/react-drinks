c132ae6bf8bdfb64fa500244bdc8dda8cfb22e0c
Sean Kelley <seansfkelley@gmail.com>
2017-07-16 13:02:21 -0700

    Use -x flag to get better logging on build script.

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env sh
 
-set -e -o pipefail
+set -ex -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIST=".dist"
@@ -8,33 +8,29 @@ DIST=".dist"
 cd "$DIR"
 cd ..
 
-echo "building from $(pwd)"
-
-echo "emptying previous dist folder..."
+# init
 
 rm -r "$DIST"
 mkdir "$DIST"
 
-echo "building scripts..."
+# js
 
-BROWSERIFY='./node_modules/.bin/browserify --extension=tsx --extension=ts --extension=js -p tsify'
+BROWSERIFY_COMMAND='./node_modules/.bin/browserify --extension=tsx --extension=ts --extension=js -p tsify'
 
-$BROWSERIFY frontend/endpoints/recipe/recipe-init.tsx  -o "$DIST/recipe-init.js"
-$BROWSERIFY frontend/endpoints/app/app-init.tsx  -o "$DIST/app-init.js"
+$BROWSERIFY_COMMAND frontend/endpoints/recipe/recipe-init.tsx  -o "$DIST/recipe-init.js"
+$BROWSERIFY_COMMAND frontend/endpoints/app/app-init.tsx  -o "$DIST/app-init.js"
 
-echo "building styles..."
+# css
 
 ALL_STYLES="$DIST/all-styles.css"
 
 touch "$ALL_STYLES"
 cat ./node_modules/font-awesome/css/font-awesome.css >> "$ALL_STYLES"
 ./node_modules/stylus/bin/stylus styles/index.styl -p >> "$ALL_STYLES"
-cat "$ALL_STYLES" | sed -e 's/\.\.\/fonts\//\.\/fonts\//g' > "$ALL_STYLES"
+sed -i '' -e 's/\.\.\/fonts\//\.\fonts\//g' "$ALL_STYLES"
 
-echo "copying static assets..."
+# static
 
 cp -r fonts "$DIST"
 cp -r ./node_modules/font-awesome/fonts "$DIST"
 cp -r img "$DIST"
-
-echo "done!"
